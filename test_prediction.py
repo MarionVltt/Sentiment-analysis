15957aa0c03edf520197709127fccb68e1f7a3e0
MarionVltt <marion.valette11@gmail.com>
2021-02-25 11:44:04 +0100

    delete files

--- test_prediction.py
+++ test_prediction.py
@@ -1,84 +1,46 @@
-import preprocessing_en
-import preprocessing_fr
-import prediction_en
-import prediction_fr
+# usr/bin/python3
+
+# Prediction test file, uses SingleRaw and SinglePredict from preprocessing.py and prediction.py
+
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
-import time
-import json
 
-###
-raw = "Good morning, I'm 21 and my web <br /><br /> .site is www.fakewebsite.com or https://www.fakewebsite.com!!"
-new_raw = preprocessing_en.SingleRaw(raw)
-new_raw.standardize_raw()
-print(new_raw)
-# file_path = "books-fr.test.csv"
-# new_raw = preprocessing_fr.RangeRaw(file_path, 0)
-# new_raw.standardize_range()
-# print(new_raw.df.shape)
-# print(new_raw.df.head())
-#
-# file_path = "dataAmazonElecSmall.csv"
-# new_raw = preprocessing_en.RangeRaw(file_path, 1)
-# new_raw.standardize_range()
-# print(new_raw.df.shape)
-# print(new_raw.df.head())
-####
-t = time.time()
-prediction_en.SinglePredict.initialize(model_path="model")
-t0 = time.time()
-new_predict = prediction_en.SinglePredict("wwww.absurde.com", ["Neg", "Pos"], sequence_to_remove=["book", "is"])
-t1 = time.time()
+import preprocessing
+import prediction
+
+# Define sentence
+sentence = "Good morning, I'm 21 and my web <br /><br /> .site is www.fakewebsite.com or https://www.fakewebsite.com!!"
+
+# Initialization and preprocessing
+prediction.SinglePredict.initialize(model_path="model")
+new_predict = prediction.SinglePredict(
+    sentence, ["Neg", "Pos"], sequence_to_remove=["book", "is"])
 new_predict.preprocess()
-print(new_predict.clean_text)
-t2 = time.time()
-t3 = time.time()
+print("Preprocesed text : ", new_predict.clean_text)
+
+# Prediction
 if not new_predict.default_case:
-    t4 = time.time()
-    times1 = new_predict.predict()
-    results1 = new_predict.predicted_class
-    t5 = time.time()
+    new_predict.predict()
+    result = new_predict.predicted_class
+    print("Predicted class : ", result)
 else:
-    t4 = time.time()
-    t5 = time.time()
-    results1 = None
+    results = None
     print("default case : no prediction")
-#print("First done")
-#new_predict = prediction_en.SinglePredict("I'm all for saving money on a case but this case was absolutely cheap and flimsy.  Broke within a week and just all around not a great buy.  The silicone doesn't stay in place either.", ["Neg", "Pos"],model_path="model_dir")
-t5bis = time.time()
-new_predict = prediction_en.SinglePredict("The film was great", ["Negative", "Positive"])
-t6 = time.time()
+
+# Try a second sentence
+new_predict = prediction.SinglePredict(
+    "The film was great", ["Negative", "Positive"])
 new_predict.preprocess()
-t7 = time.time()
 if not new_predict.default_case:
-    t8 = time.time()
-    times2 = new_predict.predict()
-    results2 = new_predict.predicted_class
-    t9 = time.time()
+    new_predict.predict()
+    new_result = new_predict.predicted_class
+    print("Predicted class : ", new_result)
 else:
-    t8 = time.time()
-    t9 = time.time()
-    results2 = None
+    new_result = None
     print("default case : no prediction")
-new_predict = prediction_en.SinglePredict("It is great here, I love it", ["Neg", "Pos"])
+
+# Try a third sentence, with the global function clean_and_predict
+new_predict = prediction.SinglePredict(
+    "It is great here, I love it", ["Neg", "Pos"])
 new_predict.clean_and_predict()
-prediction_en.SinglePredict.unload()
-print("Time init : ", t0-t)
-print("Total time needed : ", t5-t0)
-print("Time creation 1: ", t1-t0)
-print("Time clean : ", t2-t1)
-print("Time to features : ", t4-t3)
-print("Time predict : ", t5-t4)
-print("\n")
-print("Total time needed : ", t9-t5bis)
-print("Time creation 2: ", t6-t5bis)
-print("Time clean : ", t7-t6)
-print("Time features : ", t8-t7)
-print("Time predict : ", t9-t8)
-print(results1, results2)
 
-
-print(new_predict.input_features[0].input_ids)
-print(new_predict.input_features[0].input_mask)
-print(new_predict.input_features[0].label_id)
-print(new_predict.input_features[0].segment_ids)
-
+prediction.SinglePredict.unload()
--- test_train.py
+++ test_train.py
@@ -1,11 +1,14 @@
-import prediction_en
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
-import time
-import pandas as pd
-import numpy as np
+# usr/bin/python3
+
 import os
 
-print(os.getcwd())
+import pandas as pd
+import numpy as np
+
+import prediction
+
+
+# Load data
 df = pd.read_csv("data/data_en.csv", sep=";", header=None, index_col=0)
 df.columns = ['Tag', 'Phrase']
 df = df.reindex(np.random.permutation(df.index))
@@ -13,19 +16,10 @@ print(df.shape)
 print(df.head())
 df_small = df.sample(n=10)
 print(df_small.Tag.value_counts())
+
+# Training
 print("Preprocesss and training")
 rp = prediction_en.RangePredict(df_small, 1, 0)
 rp.preprocess()
 rp.train()
 rp.export()
-# df_test = pd.read_csv("dataAmazonElecSmall2.csv", sep=";", header=None, index_col=0)
-# print(df_test.head())
-# #df_test.drop(['clean_text'], inplace=True, axis=1)
-# df_test.columns = ['Tag', 'Phrase']
-# df_test = df_test.reindex(np.random.permutation(df_test.index))
-# df_test.dropna(inplace=True)
-# #df_test = df_test.sample(n=10000)
-# print(df_test.shape)
-# print(df_test.head())
-# print("Evaluating generalization")
-# results = rp.predict_estimator_test(df_test['Phrase'], df_test['Tag'])
